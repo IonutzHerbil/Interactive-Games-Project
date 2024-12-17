d4e4550748e6f07ba426417ede6733fb3921bcc7
Herbil Ioan <ionutherbil@gmail.com>
2024-12-17 21:40:40 +0200

    corrections in CSS

--- interactive-frontend/src/pages/MemoryMatrix/Square.jsx
+++ interactive-frontend/src/pages/MemoryMatrix/Square.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './MemoryMatrix.css';
+import '../../styles/MemoryMatrix.css';
 import Square from './Square';
 
 function MemoryMatrix() {
